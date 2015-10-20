b21475adabad089b090ce8678a1eb79c4d344e55
Kyle Travis <kyletravis@users.noreply.github.com>
2015-10-20 12:01:47 -0400

    Removed Sleuth from Web service

--- springboottrades-web/src/main/java/io/pivotal/web/WebApplication.java
+++ springboottrades-web/src/main/java/io/pivotal/web/WebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+// import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -14,10 +14,10 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableCircuitBreaker
 public class WebApplication {
 
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
+//    @Bean
+//    public AlwaysSampler defaultSampler() {
+//        return new AlwaysSampler();
+//    }
 	
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
