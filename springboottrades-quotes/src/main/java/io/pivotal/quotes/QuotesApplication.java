4b233809bb742330399d2bf1504b8d99307ee368
Kyle Travis <kyletravis@users.noreply.github.com>
2015-10-20 12:00:24 -0400

    Removed Sleuth from Quotes service

--- springboottrades-quotes/src/main/java/io/pivotal/quotes/QuotesApplication.java
+++ springboottrades-quotes/src/main/java/io/pivotal/quotes/QuotesApplication.java
@@ -2,7 +2,7 @@ package io.pivotal.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+// import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
@@ -19,10 +19,10 @@ import org.springframework.context.annotation.Bean;
 @EnableDiscoveryClient
 public class QuotesApplication {
 
-	@Bean
-	public AlwaysSampler defaultSampler() {
-		return new AlwaysSampler();
-	}
+//	@Bean
+//	public AlwaysSampler defaultSampler() {
+//		return new AlwaysSampler();
+//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(QuotesApplication.class, args);
