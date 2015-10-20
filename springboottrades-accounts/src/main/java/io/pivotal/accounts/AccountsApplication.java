26e370fab33bbb2d3491db3d0935b53c50ae7c5f
Kyle Travis <kyletravis@users.noreply.github.com>
2015-10-20 11:53:25 -0400

    Removed Sleuth

--- springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
+++ springboottrades-accounts/src/main/java/io/pivotal/accounts/AccountsApplication.java
@@ -3,7 +3,7 @@ package io.pivotal.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+// import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 
 /**
