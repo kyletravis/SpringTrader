8447baa65876d37972150af0cd1640ccbe1de591
Kyle Travis <kyletravis@users.noreply.github.com>
2015-10-20 11:48:35 -0400

    appended "-kyle" to app names

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cf delete webtrader -f
-cf delete portfolio -f
-cf delete accounts -f
-cf delete quotes -f
+cf delete webtrader-kyle -f
+cf delete portfolio-kyle -f
+cf delete accounts-kyle -f
+cf delete quotes-kyle -f
 
 cf delete-service trader-db -f
 cf delete-service trader-registry -f
